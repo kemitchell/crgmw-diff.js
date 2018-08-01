dc2beae411ad763376d4f6164480922681b3150a
Kyle E. Mitchell <kyle@kemitchell.com>
2018-08-01 16:35:54 -0700

    Flatten single-test suite

--- test/postorder.test.js
+++ test/postorder.test.js
@@ -1,15 +1,13 @@
 var tape = require('tape')
 var postorder = require('../postorder')
 
-tape('postorder', function (suite) {
-  suite.test('sanity check', function (test) {
-    var tree = {
-      label: 'p',
-      children: [{label: 'a'}, {label: 'b'}, {label: 'c'}]
-    }
-    var order = []
-    postorder(tree, function (node) { order.push(node.label) })
-    test.deepEqual(order, ['a', 'b', 'c', 'p'], 'expected order')
-    test.end()
-  })
+tape('postorder sanity check', function (test) {
+  var tree = {
+    label: 'p',
+    children: [{label: 'a'}, {label: 'b'}, {label: 'c'}]
+  }
+  var order = []
+  postorder(tree, function (node) { order.push(node.label) })
+  test.deepEqual(order, ['a', 'b', 'c', 'p'], 'expected order')
+  test.end()
 })
