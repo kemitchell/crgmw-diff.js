628a32abc899824f167ae4015bac03755528d25b
Kyle E. Mitchell <kyle@kemitchell.com>
2018-08-05 10:15:12 -0700

    Test jsonolt-style array swap

--- test/diff.test.js
+++ test/diff.test.js
@@ -25,3 +25,49 @@ tape.test('diff renamed key', function (t) {
   t.deepEqual(result.editScript, expected)
   t.end()
 })
+
+tape.test('diff swapped array', function (t) {
+  var a = {label: {type: 'string', value: 'a'}}
+  var b = {label: {type: 'string', value: 'b'}}
+  var left = {
+    label: {type: 'array'},
+    children: [
+      {
+        label: indexLabel(0),
+        children: [clone(a)]
+      },
+      {
+        label: indexLabel(1),
+        children: [clone(b)]
+      }
+    ]
+  }
+  var right = {
+    label: {type: 'array'},
+    children: [
+      {
+        label: indexLabel(0),
+        children: [clone(b)]
+      },
+      {
+        label: indexLabel(1),
+        children: [clone(a)]
+      }
+    ]
+  }
+  var result = diff(left, right)
+  var editScript = result.editScript
+  t.equal(editScript.length, 3)
+  t.equal(editScript[0].operation, 'move')
+  t.equal(editScript[1].operation, 'update')
+  t.equal(editScript[2].operation, 'update')
+  t.end()
+})
+
+function indexLabel (index) {
+  return {type: 'index', value: index}
+}
+
+function clone (value) {
+  return JSON.parse(JSON.stringify(value))
+}
